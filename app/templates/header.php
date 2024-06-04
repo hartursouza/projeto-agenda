cc20d815794dc67320cb3934547320744d9563cc
Hartur Souza <hartursouza3@gmail.com>
2024-06-03 21:48:02 -0300

    commit directory app

---
+++ app/templates/header.php
@@ -0,0 +1,36 @@
+<?php 
+    include_once("../app/config/url.php");
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- BOOTSTRAP -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.3/css/bootstrap.min.css"
+        integrity="sha512-jnSuA4Ss2PkkikSOLtYs8BlYIeeIK1h99ty4YfvRPAlzr377vr3CXDb7sb7eEEBYjDtcYj+AjBH3FLv5uSJuXg=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- FONT AWESOME -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
+        integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="<?=$BASE_URL?>/css/styles.css">
+    <title>Agenda de Contatos</title>
+</head>
+
+<body>
+    <header>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+            <a class="navbar-brand" href="<?=$BASE_URL?>/index.php">
+                <img src="<?=$BASE_URL?>/images/logo.svg" alt="Agenda">
+            </a>
+            <div>
+                <div class="navbar-nav">
+                    <a class="nav-link active" id="home-link" href="<?=$BASE_URL?>/index.php">Agenda</a>
+                    <a class="nav-link active" id="home-link" href="<?=$BASE_URL?>/create.php">Novo Contato</a>
+                </div>
+            </div>
+        </nav>
+    </header>
