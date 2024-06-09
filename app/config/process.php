7987de39e78e31158464f908f8557b6d90c5d445
Hartur Souza <hartursouza3@gmail.com>
2024-06-08 21:13:17 -0300

    update home page and add show.php for show the contacts infomations

--- app/config/process.php
+++ app/config/process.php
@@ -4,14 +4,40 @@
     include_once("connection.php");
     include_once("url.php");
 
-    $contacts = [];
+    $id;
 
-    $q = "SELECT * FROM contacts";
+    if(!empty($_GET)){
+        $id = $_GET['id'];
+    }
 
-    $stmt = $conn->prepare($q);
+    //Selecionar um contato
 
-    $stmt->execute();
+    if(!empty($id)) {
 
-    $contacts = $stmt->fetchAll();
+        $query = "SELECT * FROM contacts WHERE id = :id";
+
+        $stmt = $conn->prepare($query);
+
+        $stmt->bindParam(":id", $id);
+
+        $stmt->execute();
+
+        $contact = $stmt->fetch();
+
+    } else {
+        //Selecionar todos os contatos
+
+        $contacts = [];
+
+        $q = "SELECT * FROM contacts";
+
+        $stmt = $conn->prepare($q);
+
+        $stmt->execute();
+
+        $contacts = $stmt->fetchAll();
+    }
+
+    
 
 ?>
